fb5aab805015920d2c9eb43af3d021fcff0b0e4c
parker3216 <parker3216@hotmail.com>
2022-08-31 20:30:00 +0800

    feat:add user register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
+
 router.get('/login' , (req,res) =>{
   res.render('login')
 })
@@ -12,6 +14,52 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
+router.post('/register' ,(req,res) =>{
+  const { name , email , password , confirmPassword} = req.body
+  const errors = []
+  if(!email || !password || !confirmPassword) {
+    errors.push({ message:'所有欄位都是必填！'})
+  }
+  if( password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register' ,{
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+
+  User,findOne({ email })
+  .then(user => {
+    if(user) {
+      errors.push({ message: '此Email已註冊過' })
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword
+    })
+  } 
+    
+
+
+
+
+
+
+})
+
+
+
+
+
+
+})
 
 
 
