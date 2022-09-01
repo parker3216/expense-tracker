14369601b6fb01526d2ce6c866281d2f9ff0e4e6
parker3216 <parker3216@hotmail.com>
2022-09-01 08:21:29 +0800

    feat add isAuthenticated to display different view templates and modify router&main.hbs bug

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const Record = require('../../models/record')
 
 //新增支出頁面
-app.get('/records/new', (req, res) => {
+router.get('/new', (req, res) => {
   return res.render('new')
 })
 
 //新增支出資料->將form送往database
-app.post('/records', (req, res) => {
+router.post('/', (req, res) => {
   const userId = req.user._id
   const { name, date, category, amount } = req.body
   return Record.create({ name, date, category, amount })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
+
+module.exports = router
